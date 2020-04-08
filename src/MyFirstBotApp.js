f84eff63c2edce17780eaa631b3f966b9c50fa61
Vadim Cpp <vadim.cpp@gmail.com>
2020-04-08 19:40:55 +0200

    #3 display basic information about events and services

--- src/MyFirstBotApp.js
+++ src/MyFirstBotApp.js
@@ -29,6 +29,49 @@ class MyFirstBotApp {
         console.log(' 2️⃣  [MyFirstBotApp]: Connected as ' + this._firebaseApp.name);        
     }
 
+    getInfo = (aCallback) => {
+        const db = this._firebaseApp.firestore();
+        db.collection("services").get()
+        .then(function(querySnapshot) {
+            const services = querySnapshot.docs.map(item => ({ ...item.data(), id: item.id }))
+            
+            db.collection("events").get()
+            .then(function(querySnapshot) {
+                const events = querySnapshot.docs.map(item => ({ ...item.data(), id: item.id }))
+                
+                let message = '';
+                
+                if (events.length > 0) {
+                    message += `Предстоящие мероприятия: ${events.length}\n\n`;
+                } else {
+                    message += 'Предстоящих мероприятий нет\n\n';
+                }
+
+                if (services.length > 0) {
+                    message += `Услуги: ${services.length}`;
+                } else {
+                    message += 'Услуг  нет'
+                }
+                
+                aCallback(message)
+            })
+            .catch(function(error) {
+                console.warn("Error getting services, skip: ", error);
+                aCallback(
+                    'Увы, произошла неизвестная ошибка. ' + 
+                    'Обратитесь пожалуйста в техническую поддержку: @frontendbasics'
+                );
+            });
+        })
+        .catch(function(error) {
+            console.warn("Error getting services, skip: ", error);
+            aCallback(
+                'Увы, произошла неизвестная ошибка. ' + 
+                'Обратитесь пожалуйста в техническую поддержку: @frontendbasics'
+            );
+        });
+    }
+
     /**
      * Main event handler
      *
@@ -56,18 +99,27 @@ class MyFirstBotApp {
                 'Бот следит за изменениями на сайте [events4friend.ru](https://events4friends.ru/) ' + 
                 'и обновляет информацию в закрепленном сообщении чата.\n\n' +
                 'Введите команду /info, чтобы посмотреть инормацию об услугах и мероприятиях.';
+            bot.sendMessage(msg.chat.id, messageText, {                
+                parse_mode: "Markdown",
+                disable_web_page_preview: true,                        
+            });                
+    
         } else if (messageText === '/info') {
-            messageText = 'TODO';
+            this.getInfo((aMessage) => {
+                bot.sendMessage(msg.chat.id, aMessage, {                
+                    parse_mode: "Markdown",
+                    disable_web_page_preview: true,                        
+                });                 
+            })
         } else {
             messageText =
                 'Уважаемый(ая) ' + this._getName(msg) + ".\n\n" +
                 'Введите команду /info, чтобы посмотреть инормацию об услугах и мероприятиях.';
+            bot.sendMessage(msg.chat.id, messageText, {                
+                parse_mode: "Markdown",
+                disable_web_page_preview: true,                        
+            });                
         }
-
-        bot.sendMessage(msg.chat.id, messageText, {                
-            parse_mode: "Markdown",
-            disable_web_page_preview: true,                        
-        });
     }
 
     /**
